8c85c39ab4592e17fd2cb95273b91e8631230ea2
李譯騏 <patrick14754@gmail.com>
2020-11-04 01:58:12 +0800

    修改navigation.blade.php和Index.blade.php 的超連結路由

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -8,17 +8,17 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.html">Home</a>
+                <li>
+                    <a href="{{route('posts.index')}}">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="about.html">About</a>
+                <li>
+                    <a href="{{route('posts.about')}}">About</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="post.html">Sample Post</a>
+                <li>
+                    <a href="{{route('posts.show')}}">Sample Post</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact</a>
+                <li>
+                    <a href="{{route('posts.contact')}}">Contact</a>
                 </li>
             </ul>
         </div>
